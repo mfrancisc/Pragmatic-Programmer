41be8f56bd98a2177f1137726147eed552fba98d
Francisc Munteanu <munteanu.francisc@gmail.com>
2015-12-17 00:56:39 +0100

    Refactor for adding new languages

--- ex13/CodeGenerator.php
+++ ex13/CodeGenerator.php
@@ -38,7 +38,32 @@ if(!in_array($lang , $langArray))
 
 include_once $lang .".php";
 
-$generator = new CodeGenerator\Generator($inputFileName, $outputFileName);
-$generator->output();
+$generator = new CodeGenerator\Generator();
+$handle = fopen($inputFileName, "r");
+if ($handle) {
+  while (($line = fgets($handle)) !== false) {
+    $generator->handleLine($line);
 
+    switch ($generator->getLineType()) {
+    case '#':
+      $generator->comment();
+      break;
+
+    case 'M':
+      $generator->messageDef();
+      break;
+
+    case 'F':
+      $generator->typeDef();
+      break;
+
+    case 'E':
+      $generator->messageEnd();
+      break;
+
+    }
+  }
+
+  $generator->output($outputFileName);
+}
 exit("Template generated: ". $outputFileName);
--- ex13/c.php
+++ ex13/c.php
@@ -7,52 +7,84 @@ namespace CodeGenerator;
 
 class Generator {
 
-  private $inputFileName, $outputCode, $outputFileName;
+  private  $outputCode;
+  private $line, $lineArray, $lineType;
+  private $endMsg;
 
-  public function __construct($inputFileName, $outputFileName)
+
+  /**
+   * generates the template 
+   *
+   */
+  public function output($outputFileName)
   {
-    $this->inputFileName = $inputFileName; 
-    $this->outputFileName = $outputFileName; 
+    file_put_contents($outputFileName . ".c", $this->outputCode);
   }
 
-  public function output()
+  public function debug()
   {
-    $handle = fopen($this->inputFileName, "r");
-    if ($handle) {
-      $firstCommentFound = false;
-      $newFileContent = "";
-      while (($line = fgets($handle)) !== false) {
-        $line = str_replace("\n", "", $line);
-        $lineArray =explode(" ", $line);
-        $lineType = $lineArray[0];
+    print_r($this->outputCode); 
+  }
 
-        switch ($lineType) {
-        case '#':
-        $line = str_replace("#", "", $line);
-          $this->outputCode =  $this->outputCode . "/* " . $line . " */" . PHP_EOL;
-          break;
+  /**
+   * format line
+   *
+   */
+  public function handleLine($line)
+  {
+    $this->line = str_replace("\n", "", $line);
+    $this->lineArray =explode(" ", $this->line);
+    $this->lineType = $this->lineArray[0];
+  }
 
-        case 'M':
-        $line = str_replace("M", "", $line);
-        $this->outputCode = $this->outputCode . "typedef struct {" . PHP_EOL;
-          $endMsg = "} " . $line . "Msg;";
-          break;
+  /**
+   * returns line type
+   *
+   */
+  public function getLineType()
+  {
+    return $this->lineType; 
+  }
 
-        case 'F':
-          //searcig for length definition, es: name[30]
-          preg_match("/\[[^\]]*\]/", $lineArray[2], $matches);
-          $lineArray[2] = preg_replace("/\[[^\]]*\]/", "", $lineArray[2]);
+  /**
+   * generate comment line
+   *
+   */
+  public function comment()
+  {
+    $this->line = str_replace("#", "", $this->line);
+    $this->outputCode =  $this->outputCode . "/* " . $this->line . " */" . PHP_EOL;
+  }
 
-          if(isset($matches[0]) ? $length = $matches[0] : $length = "");
- 
-          $this->outputCode = $this->outputCode . $lineArray[2] .  " " . $lineArray[1] . $length . ";" . PHP_EOL;
-          break;
+  public function messageDef()
+  {
+    $this->line = str_replace("M", "", $this->line);
+    $this->outputCode = $this->outputCode . "typedef struct {" . PHP_EOL;
+    $this->endMsg = "} " . $this->line . "Msg;";
+  }
 
-        }
-      }
-      $this->outputCode .= $endMsg;
+  /**
+   * type definition
+   * simple, array types
+   *
+   */
+  public function typeDef()
+  {
+    //searcig for length definition, es: name[30]
+    preg_match("/\[[^\]]*\]/", $this->lineArray[2], $matches);
+    $this->lineArray[2] = preg_replace("/\[[^\]]*\]/", "", $this->lineArray[2]);
 
-      file_put_contents($this->outputFileName . ".c", $this->outputCode);
-    }
+    if(isset($matches[0]) ? $length = $matches[0] : $length = "");
+
+    $this->outputCode = $this->outputCode . $this->lineArray[2] .  " " . $this->lineArray[1] . $length . ";" . PHP_EOL;
+  }
+
+  /**
+   * End of message definition
+   *
+   */
+  public function messageEnd()
+  {
+    $this->outputCode .= $this->endMsg;
   }
 }
